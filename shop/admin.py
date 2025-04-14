b747401c67575a35dcb7893e2799ab17178551e7
Ali Mohammadi <ali0182mohammadi@gmail.com>
2025-04-14 11:53:41 +0330

    administrator & signal for calculate new_price

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Product, Category, Image, ProductFeature
 
 # Register your models here.
+
+
+class ImageInline(admin.TabularInline):
+    model = Image
+    extra = 0
+
+
+class FeatureInline(admin.TabularInline):
+    model = ProductFeature
+    extra = 0
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'inventory', 'new_price', 'created', 'update']
+    prepopulated_fields = {'slug': ('name',)}
+    list_filter = ['created', 'update']
+    inlines = [ImageInline, FeatureInline]
