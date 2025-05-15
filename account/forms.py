7d1942587f1dd2b4993dff0916914b2d8af7c2b4
Ali Mohammadi <ali0182mohammadi@gmail.com>
2025-05-15 19:53:55 +0330

    clean phone

--- account/forms.py
+++ account/forms.py
@@ -8,6 +8,28 @@ class ShopUserCreationForm(UserCreationForm):
         model = ShopUser
         fields = ('phone', 'first_name', 'last_name', 'address', 'is_active', 'is_staff', 'is_superuser')
 
+    def clean_phone(self):
+        phone = self.cleaned_data.get('phone')
+
+        if self.instance.pk:
+            if ShopUser.objects.filter(phone=phone).exclude(pk=self.instance.pk).exists():
+                raise forms.ValidationError('Phone already exists.')
+        else:
+            if ShopUser.objects.filter(phone=phone).exists():
+                raise forms.ValidationError('Phone already exists.')
+
+        if not phone.isdigit:
+            raise forms.ValidationError('Phone must be a number.')
+
+        if not phone.startswith('09'):
+            raise forms.ValidationError('Phone must be start with 09 digit.')
+
+        if len(phone) != 11:
+            raise forms.ValidationError('Phone must have 11 digits.')
+
+        return phone
+
+
 
 class ShopUserChangeForm(UserChangeForm):
     class Meta(UserChangeForm.Meta):
